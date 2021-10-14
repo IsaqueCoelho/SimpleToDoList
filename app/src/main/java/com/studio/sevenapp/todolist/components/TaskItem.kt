e54ccf8bd9cfa85c5b916a138a12a180bdca72da
Isaque Coelho <isaquecoelho.trabalho@hotmail.com>
2021-10-13 22:17:16 -0300

    task: Implment TaskItem Component

---
+++ app/src/main/java/com/studio/sevenapp/todolist/components/TaskItem.kt
@@ -0,0 +1,46 @@
+package com.studio.sevenapp.todolist.components
+
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.padding
+import androidx.compose.material.Checkbox
+import androidx.compose.material.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
+
+@Composable
+fun TaskItem(
+    text: String
+) {
+    Row(
+        modifier = Modifier
+            .fillMaxWidth()
+            .padding(8.dp),
+        horizontalArrangement = Arrangement.SpaceBetween
+    ) {
+        Text(text = text)
+        TaskItemCheckbox()
+    }
+}
+
+@Composable
+fun TaskItemCheckbox() {
+    val checkedState = remember { mutableStateOf(false) }
+    Checkbox(
+        checked = checkedState.value,
+        onCheckedChange = { checkedState.value = it }
+    )
+}
+
+@Preview(
+    showBackground = true
+)
+@Composable
+fun PreviewTaskItem() {
+    TaskItem(text = "Take out the trash")
+}
