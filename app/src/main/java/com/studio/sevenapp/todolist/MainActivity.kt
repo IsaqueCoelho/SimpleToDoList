e0fcbc2cce5af178becd02fa88aa2a60b9fcfc47
Isaque Coelho <isaque.coelho12@gmail.com>
2021-12-28 22:44:23 -0300

    task: adjust annotation and upload gif to use on readme

--- app/src/main/java/com/studio/sevenapp/todolist/MainActivity.kt
+++ app/src/main/java/com/studio/sevenapp/todolist/MainActivity.kt
@@ -4,10 +4,12 @@ import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.compose.foundation.ExperimentalFoundationApi
+import androidx.compose.material.ExperimentalMaterialApi
 import com.studio.sevenapp.todolist.navigations.NavigationGraph
 import com.studio.sevenapp.todolist.ui.theme.TODoListTheme
 
 class MainActivity : ComponentActivity() {
+    @ExperimentalMaterialApi
     @ExperimentalFoundationApi
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
